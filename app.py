35d00fe260fada432e2f077a91612656ccb33526
Sunil.A.Kumar <Sunil.a.kumar@geminisolutions.com>
2025-01-22 20:23:03 +0530

    apm integration needed, pending

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/root', methods=['GET'])
 def hello_world():
     return jsonify(message="Hello, World!")
 
